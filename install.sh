9bfca68f6c9234392f8b039f39911d41cb6f1bf1
linshibo <linshibo3456@gmail.com>
2014-09-11 14:05:40 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #set bashrc
 cd `dirname $0`
-echo "if [ -f ~/myvim/bashrc ]; then \
-    . ~/myvim/bashrc \
+echo "if [ -f ~/myvim/bashrc ]; then \n
+    . ~/myvim/bashrc \n
 fi" >>~/.bashrc 
 if [ -d ~/.vim ]; then
     mv ~/.vim ~/.vim.bak
 fi
 #set vim
 ln -s ./_vim ~/.vim
-echo "source `pwd`/_vim/comm.vim" >> ~/.vimrc
+echo "source ~/.vim/comm.vim\n" >> ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git _vim/bundle/vundle
 vim -c 'BundleInstall' -c ':q!' -c ':q!'
 go get -u github.com/jstemmer/gotags
