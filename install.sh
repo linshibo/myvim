7d79ac4c664cde786fdc8beed1496c5ea5ac9b45
linshibo <linshibo@gmail.com>
2017-03-17 18:54:34 +0800

    rm .gconf

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#set bashrc
 cd `dirname $0`
-echo "source `pwd`/bashrc" >>~/.bashrc
 if [ -d ~/.vim ]; then
     mv ~/.vim ~/.vim.bak
 fi
 ln -s _vim ~/.vim
 echo "source ~/.vim/comm.vim\n" >> ~/.vimrc
-git clone https://github.com/gmarik/Vundle.vim.git _vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/vim/bundle/
 vim -c 'PluginInstall' -c ':q!' -c ':q!'
